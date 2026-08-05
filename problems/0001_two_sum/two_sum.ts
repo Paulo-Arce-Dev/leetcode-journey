79d1e37c4d71837fdc9cc4ab7faa258b5e306562
Paulo Arce <codebypaulo@gmail.com>
2026-08-05 00:26:07 -0300

    first solution

---
+++ problems/0001_two_sum/two_sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number) {
+  const vistos = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complemento = target - nums[i];
+    if (vistos.has(complemento)) {
+      return [vistos.get(complemento)!, i];
+    }
+    vistos.set(nums[i], i);
+  }
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
