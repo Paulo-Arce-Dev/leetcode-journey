6b27489724a25f79bad35f5b8b3c2cb612263f1b
Paulo Arce <215511384+Paulo-Arce-Dev@users.noreply.github.com>
2026-08-13 14:14:37 -0300

    fix: compare truncated x instead of original value to fix false negative

--- problems/0009_palindrome_number/palindrome-number.ts
+++ problems/0009_palindrome_number/palindrome-number.ts
@@ -2,12 +2,11 @@ function isPalindrome(x: number): boolean {
   if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
 
   let reverted = 0;
-  let original = x;
 
   while (x > reverted) {
     reverted = reverted * 10 + (x % 10);
     x = Math.trunc(x / 10);
   }
 
-  return original === reverted || original === Math.trunc(reverted / 10);
+  return x === reverted || x === Math.trunc(reverted / 10);
 }
